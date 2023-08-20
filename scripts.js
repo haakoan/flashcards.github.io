a772f8417c437d405861c41f02e51b51772c07ea
haakoan <haakoan@gmail.com>
2023-08-20 13:40:02 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,23 +1,41 @@
-let cards = [
-    { question: "$$5 + 3$$", answer: "$$8$$" },
-    { question: "$$\\int_0^1 x^2 dx$$", answer: "$$\\frac{1}{3}$$" },
-    // Add more cards as needed
-];
+let topics = {
+    Arithmetic: [
+        { question: "$$5 + 3$$", answer: "$$8$$" },
+        { question: "$$9 - 6$$", answer: "$$3$$" },
+    ],
+    Integration: [
+        { question: "$$\\int_0^1 x^2 dx$$", answer: "$$\\frac{1}{3}$$" },
+        // ... other integration questions
+    ],
+    // ... add more topics as needed
+};
 
+let currentTopic = 'Arithmetic'; // default topic
 let currentCard = 0;
 
-// Initialize the card with both question and answer
-updateCard();
+// Populate topic selection dropdown
+for (let topic in topics) {
+    let option = document.createElement('option');
+    option.value = topic;
+    option.textContent = topic;
+    document.getElementById('topicSelect').appendChild(option);
+}
+
+document.getElementById('topicSelect').addEventListener('change', function() {
+    currentTopic = this.value;
+    currentCard = 0;  // Reset card index when topic changes
+    updateCard();
+});
 
 document.getElementById('next').addEventListener('click', () => {
     currentCard++;
-    if (currentCard >= cards.length) currentCard = 0;  // loop back to the beginning
+    if (currentCard >= topics[currentTopic].length) currentCard = 0;  // loop back to the beginning
     updateCard();
 });
 
 document.getElementById('prev').addEventListener('click', () => {
     currentCard--;
-    if (currentCard < 0) currentCard = cards.length - 1;  // loop back to the end
+    if (currentCard < 0) currentCard = topics[currentTopic].length - 1;  // loop back to the end
     updateCard();
 });
 
@@ -26,10 +44,14 @@ document.getElementById('showAnswer').addEventListener('click', () => {
 });
 
 function updateCard() {
-    document.querySelector('.question').innerText = cards[currentCard].question;
-    document.querySelector('.answer').innerText = cards[currentCard].answer;
+    let card = topics[currentTopic][currentCard];
+    document.querySelector('.question').innerText = card.question;
+    document.querySelector('.answer').innerText = card.answer;
     document.querySelector('.answer').style.display = 'none';
 
     // Ask MathJax to typeset the updated content
     MathJax.typeset();
 }
+
+// Initialize the card for the default topic
+updateCard();
