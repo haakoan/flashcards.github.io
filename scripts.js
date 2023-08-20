1adf829e05a44635dee3cc7c04dd17bb21894a8b
haakoan <haakoan@gmail.com>
2023-08-20 18:22:12 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,6 +9,59 @@ let topics = {
         },
         // ... other questions
     ],
+    Logaritmer: [
+        {
+            type: "standard",
+            question: "Hva er grunnleggende definisjonen av en logaritme?",
+            answer: "En logaritme er den inverse operasjonen til eksponentiering. Den spør: 'Til hvilken eksponent må vi heve en base for å få et tall?'."
+        },
+        {
+            type: "standard",
+            question: "Hvis \( \lg(x) = y \), hvordan kan denne relasjonen uttrykkes i eksponentiell form?",
+            answer: "Relasjonen i eksponentiell form ville være \( 10^y = x \)."
+        },
+        {
+            type: "multipleChoice",
+            question: "Hva er verdien av \( \lg(100) \)?",
+            options: ["1", "2", "10", "100"],
+            correctOption: 1
+        },
+        {
+            type: "standard",
+            question: "Ved hjelp av logaritmeregenskapene, hvordan kan du forenkle \( \lg(m \times n) \)?",
+            answer: "\( \lg(m \times n) = \lg(m) + \lg(n) \)"
+        },
+        {
+            type: "standard",
+            question: "Hva representerer \( \ln(x) \) i forhold til base?",
+            answer: "Den naturlige logaritmen, \( \ln(x) \), er en logaritme til basen \( e \), hvor \( e \) er en irrasjonell konstant omtrent lik 2.71828."
+        },
+        {
+            type: "multipleChoice",
+            question: "Hvilken av følgende er den naturlige logaritmebasen?",
+            options: ["2", "10", "e", "Ingen av de ovennevnte"],
+            correctOption: 2
+        },
+        {
+            type: "standard",
+            question: "Hva er produktsetningen for logaritmer?",
+            answer: "\( \lg(m \times n) = \lg(m) + \lg(n) \)"
+        },
+        {
+            type: "standard",
+            question: "Hva er kvotientsetningen for logaritmer?",
+            answer: "\( \lg\left(\frac{m}{n}\right) = \lg(m) - \lg(n) \)"
+        },
+        {
+            type: "standard",
+            question: "Hva er potenssetningen for logaritmer?",
+            answer: "\( \lg(m^n) = n \times \lg(m) \)"
+        }
+    ]
+}
+
+
+    
     // ... other topics
 };
 
