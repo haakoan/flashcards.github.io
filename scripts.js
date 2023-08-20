3adf4a36579dc5b265dc4d08a3038de774ddf424
haakoan <haakoan@gmail.com>
2023-08-20 14:17:25 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -95,5 +95,14 @@ function updateCard() {
     MathJax.typeset();
 }
 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // swap elements
+    }
+}
+
+
 // Initialize the card for the default topic
+shuffleArray(topics[currentTopic]);
 updateCard();
